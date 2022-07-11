cc9959d856b41b6f532c2c7f8b84070375141baf
EPAM\Yury_Kuptsou <yury.kuptsou.mail@gmail.com>
2022-07-11 20:18:37 +0300

    move project to other folders

--- CatalogService/Model/Insert/CategoryInsertModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ViewModel.Insert
-{
-    public class CategoryInsertModel
-    {
-        public string Name { get; set; }
-        public Uri? Image { get; set; }
-        public int? ParentCategoryId { get; set; }
-    }
-}
--- CatalogService/Model/Update/CategoryUpdateModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ViewModel.Update
-{
-    public class CategoryUpdateModel
-    {
-        public string Name { get; set; }
-        public Uri? Image { get; set; }
-        public int? ParentCategoryId { get; set; }
-    }
-}
